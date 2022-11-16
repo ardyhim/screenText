9a8b0293225eb239811599b3bd860989f168f49e
Ardy Him <ardyhimawan@gmail.com>
2022-11-16 16:18:43 +0000

    tts

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,27 @@
 <script>
   import Synth from "./lib/Synth.svelte";
-  let message ="";
+  let message = "";
 
   let timer;
+  const synth = window.speechSynthesis;
 
-	const debounce = v => {
-		clearTimeout(timer);
-		timer = setTimeout(() => {
-			message = v;
+  let voices = synth.getVoices();
+
+  const debounce = (v) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
       console.log(v);
-		}, 750);
-	}
+      message = v.target.value;
+    }, 750);
+  };
+
+  function speech(){
+    console.log("speech")
+    const utterThis = new SpeechSynthesisUtterance(message);
+    console.log(voices[1]);
+    utterThis.voice = voices[159];
+    synth.speak(utterThis);
+  }
 </script>
 
 <main>
@@ -19,18 +30,17 @@
       <div class="absolute bottom-2/4 w-full">
         <h1 class="text-center text-6xl whitespace-pre-wrap">{message}</h1>
       </div>
-      
     </div>
-    <div class="absolute bottom-0 rounded-t-lg bg-zinc-800 w-full h-auto">
-      <form on:submit|preventDefault={debounce}>
-      <div class="mx-5 my-5 text-center">
-        <textarea class="rounded p-2 w-full text-xl" bind:value={message}></textarea>
+    <div class="absolute bottom-0 p-3 rounded-t-lg bg-zinc-800 w-full h-auto">
+      <div class="flex">
+        <div class="grow ">
+          <textarea class="rounded p-2 w-full text-xl" on:input={debounce} />
+        </div>
+        <div class=""><button on:click={speech} class="h-auto m-2 p-3 text-center rounded bg-zinc-500"> TTS </button></div>
       </div>
-      </form>
     </div>
   </div>
 </main>
 
 <style>
-  
 </style>
